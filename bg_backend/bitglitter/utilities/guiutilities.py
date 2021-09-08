91ee405de009a22b92dd523e9f016313e8926792
Mark Michon <markkmichon@gmail.com>
2021-09-07 20:01:51 -0500

    9/7 update - some starting work on GUI utility

--- bg_backend/bitglitter/utilities/guiutilities.py
+++ bg_backend/bitglitter/utilities/guiutilities.py
@@ -1,1 +1,22 @@
-# This module may not be needed, as the various function modules in config may be sufficient.  We'll see.
+from os.path import isdir, isfile
+from pathlib import Path
+
+
+def get_initial_write_data(file_path):
+    path = Path(file_path)
+    if isdir(file_path):
+        directory_scan(path)
+    elif isfile(file_path):
+        return 1, path.stat().st_size
+
+
+def directory_scan(path, current_file_count=0, current_byte_count=0):
+    return current_file_count, current_byte_count
+
+
+def file_scan(path):
+    return path.stat().st_size
+
+
+d = get_initial_write_data('/home/m/Desktop/hadouken.gif')
+print(d)
